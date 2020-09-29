339ec4521836de0e1dee3f57b124289bfc484028
Angel Pedroza <angel.pedroza@rappi.com>
2020-09-29 18:20:10 -0500

    [Fix]Change the logic

--- 0x0C-Javascript_advanced/11-prime.js
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -1,13 +1,22 @@
 function countPrimeNumbers() {
-    return 25;
-    //N of prime numbers from 2 to 100 :v
+  let count = 0;
+  let primo;
+  for (let i = 2; i <= 100; i++) {
+    primo = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) {
+        primo = false;
+        break;
+      }
+    }
+    if (primo) { count += 1;}
   }
-  let start = window.performance.now();
-  
-  for (let i = 0; i < 100; i++) {
-    console.log(countPrimeNumbers());
-  }
+  return count;
+}
 
-  let finish = window.performance.now();
-  let miliseconds = finish - start;
-  console.log("Execution time of printing countPrimeNumbers was " + miliseconds + " miliseconds");
+const t0 = performance.now();
+for (let index = 0; index < 100; index++) {
+  countPrimeNumbers();
+}
+const t1 = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${t1 - t0} milliseconds.`);
